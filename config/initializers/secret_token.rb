689d57d74c0fe26654dfe32cd2509fe50e7a32ea
John Watson <jwatson@ubuntu.(none)>
2012-12-26 18:37:34 -0800

    Bare rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swarmcombat::Application.config.secret_token = 'a3d6e419837ead73ca7fc7568c0ab5097b54e9ef86310d836a4bb4615e9db7af83c5bc26635d466913eec50f258cd166831f6eaed3af5f3449423643d105a9b3'
