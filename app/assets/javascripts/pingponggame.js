cb2eb05d4e5e373bfd0946266716d9e6dc4e4144
Keerthik <keerthiko@gmail.com>
2013-01-04 01:02:28 -0800

    Testing vector artwork, starting vector library

--- app/assets/javascripts/pingponggame.js
+++ app/assets/javascripts/pingponggame.js
@@ -8,34 +8,40 @@ function RunGame() {
 
 //Paddles
 function CreatePaddles() {
-	Crafty.c("CustomPaddle", {
+	Crafty.c("Triangle", {
 		ready:true,
 		init:function (){
 		},
-		_draw:function (ctx, po){
+		_draw:function (ctx, po, angle){
 			var pos = {	x: po._x,
 						y: po._y,
 						w: po._w,
 						h: po._h
 						};
+			var size = 20;
+			// Draw triangle facing angle from x-axis, of dimension size
+			var facing = new Crafty.math.Vector2D(size, 0);
+			var point = new Crafty.math.Vector2D(	facing.x*Math.cos(angle) - facing.y*Math.sin(angle),
+													facing.x*Math.sin(angle) + facing.y*Math.cos(angle));
 			ctx.lineWidth = 2;
 			ctx.strokeStyle = 'rgb(255,0,0)';
 			ctx.beginPath();
-			ctx.moveTo(pos.x, pos.y);
-			ctx.lineTo(pos.x + pos.w, pos.y);
-			ctx.lineTo(pos.x + pos.w, pos.y + 15);
+			ctx.moveTo(pos.x - point.x/2 + point.y/2, pos.y - point.x/2);
+			ctx.lineTo(pos.x - point.x/2 - point.y/2, pos.y + point.x/2);
+			ctx.lineTo(pos.x + point.x/2, pos.y + point.y);
+			ctx.lineTo(pos.x - point.x/2 + point.y/2, pos.y - point.x/2);
 			ctx.stroke();
 		},
 	});
-	Crafty.e("Paddle, 2D, Canvas, Color, Multiway")
+	Crafty.e("Trident, 2D, Canvas, Triangle, Multiway")
 		.bind('Draw', function (e) {
 			// Custom Vector art
-			//this._draw(e.ctx, e.pos);
+			this._draw(e.ctx, e.pos,Math.PI);
 		})
-		.color('rgb(255,0,0)')
-		.attr({x: 20, y: 100, w: 10, h: 100 })
+		//.color('rgb(255,0,0)')
+		.attr({x: 20, y: 20, w: 20, h: 20 })
 		.multiway(4, { W: -90, S: 90 });
-	Crafty.e("Paddle, 2D, Canvas, Color, Multiway")
+	Crafty.e("Diamondback, 2D, Canvas, Color, Multiway")
 		.color('rgb(0,255,0)')
 		.attr({ x: 580, y: 100, w: 10, h: 100 })
 		.multiway(4, { UP_ARROW: -90, DOWN_ARROW: 90 });
