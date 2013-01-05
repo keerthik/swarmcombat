efd9e5c133ffbc7d43c30e4b95266e00bee8b557
John Watson <jwatson@ubuntu.(none)>
2013-01-04 22:24:21 -0800

    Added open game list and game creation.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Swarmcombat::Application.routes.draw do
+
+  match '/games/delete_all_games', :controller => 'games', :action => 'delete_all_games'
   resources :games
 
   get "home/index"
@@ -52,7 +54,7 @@ Swarmcombat::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'games#index'
 
   # See how all your routes lay out with "rake routes"
 
