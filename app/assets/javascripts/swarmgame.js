9ffc5a156e2a55e2807990e40b54bb1536576920
Keerthik <keerthiko@gmail.com>
2013-01-06 02:33:48 -0800

    We got a squad that will follow code

--- app/assets/javascripts/swarmgame.js
+++ app/assets/javascripts/swarmgame.js
@@ -2,7 +2,7 @@ function RunGame() {
 	Crafty.init(600, 300);
 	Crafty.background('rgb(0,0,0)');
 	CreateDrones();
-	CreateBall();
+	//CreateBall();
 	//CreateScoreBoards();
 }
 
@@ -15,20 +15,28 @@ function CreateDrones() {
 		hp:0,
 		maxhp:100,
 		instructions:"this.facing += 0.05;",
+		color:'rgb(0,255,0)',
 		init:function (){
 			this.hp = this.maxhp;
+			this.bind('Draw', function (e) {
+				// Custom Vector art
+				this._draw(e.ctx, e.pos);
+			});
+			this.bind('EnterFrame', function (e) {
+				this._react();
+			});
+			this.bind('KeyUp', function(e) {
+				this._keyup(e);
+			});
 		},
 		_draw: function (ctx, po){
-			var pos = {	x: po._x,
-						y: po._y,
-						w: po._w,
-						h: po._h
-						};
-			var size = 20;
+			var pos = {	x: po._x, y: po._y};
+			var size = po._w;
 			// Draw triangle facing angle from x-axis, of dimension size
 			var point = drxnVector(this.facing, size);
-			drawTriangle(ctx, pos, point, 'rgb(0,255,0)');
-			drawHp(ctx, pos, size, this.hp, this.maxhp, 'rgb(255,0,0)');
+			this.color = (this.owner > 0)?'rgb(255,0,0)':'rgb(0,255,0)';
+			drawTriangle(ctx, pos, point, this.color);
+			drawHp(ctx, pos, size, this.hp, this.maxhp, this.color);
 		},
 		_triggerKey:{13:1},
 		_keyup: function (e) {
@@ -37,31 +45,21 @@ function CreateDrones() {
 			}
 		},
 		_react: function () {
-			//console.log($('#greenstruction').val());
+			// Apparently this is necessary to make the draw function work
+			this.x += 0;
 			eval(this.instructions);
 		}
 	});
-	Crafty.e("Trident, 2D, Canvas, Drone")
-		.bind('Draw', function (e) {
-			// Custom Vector art
-			this._draw(e.ctx, e.pos);
-		})
-		.bind('EnterFrame', function () {
-			this._react();
-		})
-		.bind('KeyUp', function(e) {
-			this._keyup(e);
-		})
-		.attr({x: 20, y: 20, w: 20, h: 20 });
-	Crafty.e("Diamondback, 2D, Canvas, Color, Multiway")
-		.color('rgb(0,255,0)')
-		.attr({ x: 580, y: 100, w: 10, h: 100 })
-		.multiway(4, { UP_ARROW: -90, DOWN_ARROW: 90 });
+	for (var i = 0; i < 5; i++) {
+		Crafty.e("2D, Canvas, Drone")
+			.attr({x: 20, y: 40*(i+1), w: 20, h: 20, owner: 0});
+	}
+	
 }
 //Ball
 function CreateBall() {
-	Crafty.e("2D, Canvas, Color, Collision")
-		.color('rgb(0,255,255)')
+	Crafty.e("2D, Canvas")
+		//.color('rgb(0,255,255)')
 		.attr({ x: 300, y: 150, w: 10, h: 10, 
 				dX: Crafty.math.randomInt(2, 5), 
 				dY: Crafty.math.randomInt(2, 5) })
@@ -83,18 +81,15 @@ function CreateBall() {
 
 			this.x += this.dX;
 			this.y += this.dY;
-		})
-		.onHit('Paddle', function () {
-			this.dX *= -1;
-		})
+		});
 }
 //Score boards
 function CreateScoreBoards() {
-	Crafty.e("LeftPoints, DOM, 2D, Text")
+	Crafty.e("LeftPoints, Canvas, 2D, Text")
 		.attr({ x: 20, y: 20, w: 100, h: 20, points: 0 })
 		.text("0 Points")
 		.textColor('#FF0000');
-	Crafty.e("RightPoints, DOM, 2D, Text")
+	Crafty.e("RightPoints, Canvas, 2D, Text")
 		.attr({ x: 515, y: 20, w: 100, h: 20, points: 0 })
 		.text("0 Points")
 		.textColor('#00FF00');
