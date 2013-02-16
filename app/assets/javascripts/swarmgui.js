77c64cd5c63b6d850487c475f03e1b9aa7f8f8fa
jwatson1 <jwatson1@outlook.com>
2013-02-15 16:59:45 -0800

    Fix compile function

--- app/assets/javascripts/swarmgui.js
+++ app/assets/javascripts/swarmgui.js
@@ -165,7 +165,7 @@ function CompileCode() {
 	var tempCode = "";
 	units.each( function (index) {
 		tempCode += "if(" + $(this).children(".condition").val() + ")\n";
-		tempCode += "\t" + $(this).children(".action").val() + ";\nelse";
+		tempCode += "\t" + $(this).children(".action").val() + ";\n else";
 	});
 	tempCode += "{}";
 	console.log(tempCode);
