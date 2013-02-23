d388458687282565897e088d4ad58ce20e7bbef3
Keerthik <keerthiko@gmail.com>
2013-02-23 15:47:47 -0800

    Documentation stuff

--- app/assets/javascripts/api_docs.js
+++ app/assets/javascripts/api_docs.js
@@ -1,4 +1,63 @@
 var docs = {
+	// Macro actions
+	Retreat: {
+		format: 'action',
+		type: '',
+		docstring: 'The drone will try to maneauver away from all the enemy units, giving priority to evading the closest enemies',
+		code: '',
+	},
+
+	Regroup: {
+		format: 'action',
+		type: '',
+		docstring: 'The drone will try to maneauver towards the general location of its allies',
+		code: '',
+	},
+
+	// Core actions
+	Attack: {
+		format: 'action',
+		type: '',
+		parametric: true,
+		parameters: ['drone'],
+		docstring: 'The drone will attempt to attack the target drone.\n If the target is not in range, the drone will try to maneauver into range first',
+		code: '',
+	},
+
+	moveTowardsUnit: {
+		format: 'action',
+		type: '',
+		parametric: true,
+		parameters: ['drone'],
+		docstring: 'The drone will maneauver towards the target drone',
+		code: '',
+	},
+	
+	LookAt: {
+		format: 'action',
+		type: '',
+		parametric: true,
+		parameters: ['number', 'number'],
+		docstring: 'The drone will rotate to face the target location',
+		code: '',
+	},
+
+	moveTo: {
+		format: 'action',
+		type: '',
+		parametric: true,
+		parameters: ['number', 'number'],
+		docstring: 'The drone will maneauver towards the target location',
+		code: '',
+	},
+
+	moveFd: {
+		format: 'action',
+		type: '',
+		docstring: 'The drone will advance in the direction it is facing, within pathing constraints',
+		code: '',
+	},
+
 	self: {
 		format: 'value',
 		type: 'drone.data',
@@ -24,14 +83,14 @@ var docs = {
 	FirstLiveEnemy: {
 		format: 'value',
 		type: 'drone',
-		docstring: 'The drone object of a live enemy unit. All allied drones will have the same value for this expression. This value remains unchanged until the resultant drone dies.',
+		docstring: 'The drone object of a live enemy unit.\n All allied drones will have the same value for this expression.\n This value remains unchanged until the resultant drone dies',
 		code: '',
 	},
 
 	FirstLiveAlly: {
 		format: 'value',
 		type: 'drone',
-		docstring: 'The drone object of a live allied unit. All allied drones except one (itself) will have the same value for this expression. That one drone will see the value of the next live drone besides itself. This value remains unchanged until the resultant drone dies.',
+		docstring: 'The drone object of a live allied unit.\n All allied drones except one (itself) will have the same value for this expression.\n That one drone will see the value of the next live drone besides itself.\n This value remains unchanged until the resultant drone dies',
 		code: '',
 	},
 
@@ -45,7 +104,7 @@ var docs = {
 	WeakestAlly:{
 		format: 'value',
 		type: 'drone',
-		docstring: 'The drone object of the live allied unit with the lowest hitpoints remaining, thats not the drone itself',
+		docstring: 'The drone object of the live allied unit with the lowest hitpoints remaining, thats not the drone itself.\n ',
 		code: '',
 	},
 
@@ -61,8 +120,8 @@ var docs = {
 		format: 'value',
 		type: 'number',
 		parametric: true,
-		parameters: ['number']
-		docstring: 'The number of live enemy units within the input radius units from itself. For reference, the size of the battlefield is %d by %d units',
+		parameters: ['number'],
+		docstring: 'The number of live enemy units within the input radius units from itself.\n For reference, the size of the battlefield is %w by %h units',
 		code: '',
 	},
 
@@ -70,22 +129,22 @@ var docs = {
 		format: 'value',
 		type: 'number',
 		parametric: true,
-		parameters: ['number']
-		docstring: 'The number of live allied units within the input radius units from itself. Does not include self. For reference, the size of the battlefield is %d by %d units',
+		parameters: ['number'],
+		docstring: 'The number of live allied units within the input radius units from itself.\n Does not include self.\n For reference, the size of the battlefield is %w by %h units',
 		code: '',
 	},
 	
-	EnemiesLeft {
+	EnemiesLeft: {
 		format: 'value',
 		type: 'number',
-		docstring: 'The number of live enemy units remaining on the battle field.',
+		docstring: 'The number of live enemy units remaining on the battle field.\n',
 		code: '',
 	},
 
-	AlliesLeft {
+	AlliesLeft: {
 		format: 'value',
 		type: 'number',
-		docstring: 'The number of live enemy units remaining on the battle field. Does not include self.',
+		docstring: 'The number of live enemy units remaining on the battle field.\n Does not include self',
 		code: '',
 	},
 
@@ -93,69 +152,9 @@ var docs = {
 		format: 'value',
 		type: 'number',
 		parametric: true,
-		parameters: ['drone']
-		docstring: 'The number of units of distance to an input drone. May be ally or enemy. For reference, the size of the battlefield is %d by %d units',
+		parameters: ['drone'],
+		docstring: 'The number of units of distance to an input drone.\n May be ally or enemy.\n For reference, the size of the battlefield is %w by %h units',
 		code: '',
 	},
-
-	// Macro actions
-	Retreat: {
-		format: 'action',
-		type: '',
-		docstring: 'The drone will try to maneauver away from all the enemy units, giving priority to evading the closest enemies',
-		code: '',
-	},
-
-	Regroup
-		format: 'action',
-		type: '',
-		docstring: 'The drone will try to maneauver towards the general location of its allies',
-		code: '',
-	},
-
-	// Core actions
-	Attack: {
-		format: 'action',
-		type: '',
-		parametric: true,
-		parameters: ['drone']
-		docstring: 'The drone will attempt to attack the target drone. If the target is not in range, the drone will try to maneauver into range first.',
-		code: '',
-	},
-
-	moveTowardsUnit: {
-		format: 'action',
-		type: '',
-		parametric: true,
-		parameters: ['drone']
-		docstring: 'The drone will maneauver towards the target drone.',
-		code: '',
-	},
-	
-	LookAt: {
-		format: 'action',
-		type: '',
-		parametric: true,
-		parameters: ['number', 'number'],
-		docstring: 'The drone will rotate to face the target location.',
-		code: '',
-	},
-
-	moveTo: {
-		format: 'action',
-		type: '',
-		parametric: true,
-		parameters: ['number', 'number']
-		docstring: 'The drone will maneauver towards the target location',
-		code: '',
-	},
-
-	moveFd: {
-		format: 'action',
-		type: '',
-		docstring: 'The drone will advance in the direction it is facing, within pathing constraints',
-		code: '',
-	},
-
 
 };
